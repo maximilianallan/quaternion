a3dc9585d7cdbe977b837d07e5dc011861c9242c
maximilianallan <maximilian.allan@gmail.com>
2013-06-28 11:17:23 +0100

    moved to header file only

--- inc/quaternion.hpp
+++ inc/quaternion.hpp
@@ -41,11 +41,11 @@ namespace sv {
 
     Quaternion(){}
     Quaternion(const boost::math::quaternion<double> &x):internal_quaternion_(x) {}
-    Quaternion(const double angle, const cv::Vec3f &axis);
+    inline Quaternion(const double angle, const cv::Vec3f &axis);
     inline explicit Quaternion(const cv::Vec3f &euler_angles);
-    static Quaternion FromVectorToVector(const cv::Vec3f &from, const cv::Vec3f to);
-    cv::Vec3f RotateVector(const cv::Vec3f &to_rotate) const ;
-    Quaternion Normalize() const ;
+    inline static Quaternion FromVectorToVector(const cv::Vec3f &from, const cv::Vec3f to);
+    inline cv::Vec3f RotateVector(const cv::Vec3f &to_rotate) const ;
+    inline Quaternion Normalize() const ;
     inline Quaternion Inverse() const;
     inline double X() const;
     inline double Y() const;
@@ -120,6 +120,60 @@ namespace sv {
     q.internal_quaternion_ = a.internal_quaternion_ * b.internal_quaternion_;
     return q;
   }
+  
+  
+Quaternion::Quaternion(const double angle, const cv::Vec3f &axis):internal_quaternion_(angle,axis[0],axis[1],axis[2]){}
+
+Quaternion Quaternion::FromVectorToVector(const cv::Vec3f &from, const cv::Vec3f to){
+  
+  cv::Vec3f from_n,to_n;
+  cv::normalize(from,from_n);
+  cv::normalize(to,to_n);
+      
+  float d = from_n.dot(to_n);
+
+  if(d >= 1.0){
+    return boost::math::quaternion<double>();
+  }
+
+  //check if d \approx = 0
+
+  float s = (float)sqrt( (1+d)*2 );
+  float inv_s = 1.0f/s;
+
+  cv::Vec3f axis = from_n.cross(to_n);
+
+  Quaternion q( s*0.5f, cv::Vec3f(axis[0]*inv_s, axis[1]*inv_s, axis[2]*inv_s ));
+
+  return q.Normalize();
+
+}
+
+Quaternion Quaternion::Normalize() const {
+  
+  //Note boost::math::norm(quaternion) is Cayley norm NOT Euclidean norm...
+  double mag_2 = (internal_quaternion_.R_component_1() * internal_quaternion_.R_component_1()) 
+    + (internal_quaternion_.R_component_2() * internal_quaternion_.R_component_2()) 
+    + (internal_quaternion_.R_component_3() * internal_quaternion_.R_component_3()) 
+    + (internal_quaternion_.R_component_4() * internal_quaternion_.R_component_4());
+      
+  double mag = sqrt(mag_2);
+      
+  return boost::math::quaternion<double>(internal_quaternion_.R_component_1()/mag,
+                                         internal_quaternion_.R_component_2()/mag,
+                                         internal_quaternion_.R_component_3()/mag,
+                                         internal_quaternion_.R_component_4()/mag);
+}
+
+cv::Vec3f Quaternion::RotateVector(const cv::Vec3f &to_rotate) const {
+   
+  const boost::math::quaternion<double> vec_quat(0,to_rotate[0],to_rotate[1],to_rotate[2]);
+
+  boost::math::quaternion<double> rotated = (internal_quaternion_ * vec_quat) * boost::math::conj<double>(internal_quaternion_);
+
+  return cv::Vec3f((float)rotated.R_component_2(),(float)rotated.R_component_3(),(float)rotated.R_component_3());
+
+}
 
   double Quaternion::X() const {
     return internal_quaternion_.R_component_2();
